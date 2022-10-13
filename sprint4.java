c2f641f3de5dfbc155828bc822eda6a7e1d2e5d8
Yann Santana <testeyann@hotmail.com>
2022-10-13 13:21:14 -0300

    1.0

--- sprint4.java
+++ sprint4.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class sprint4 {
     public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
-        int qtdEqp;
+        int qtdEqp, auxMa, auxMe;
         Random gera = new Random();
 
         System.out.println("Informe a quantidade de equipes para o combate TODOS X TODOS");
@@ -13,12 +13,13 @@ public class sprint4 {
         int[]numEqp=new int[qtdEqp];
         int[]qtdP=new int[qtdEqp];
         for (int i = 0; i < qtdEqp; i++) {
+            numEqp[i]=(i+1);
             qtdP[i]=gera.nextInt(1,10);
-            System.out.println("equipe "+(i+1)+" obteve "+ qtdP[i]+" pontos no primeiro combate");
+            System.out.println("equipe "+(numEqp[i])+" obteve "+ qtdP[i]+" pontos no primeiro combate");
         }
-       for (int i = 0; i <= qtdEqp; i++) {
-        if (qtdP[i]>qtdP[i+1]) {
-            System.out.println("Em "+(i)+"° Lugar equipe#");
+       for (int i = 0; i < qtdP[i]; i++) {
+        if (qtdP[i]>qtdP[i+1]||qtdP[i+1]>qtdP[i]) {
+            System.out.println("Em "+(i+1)+"° Lugar equipe "+(numEqp[i])+" com "+qtdP[i]+" Pontos");
         }
        }
 
