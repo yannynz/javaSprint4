63f76941854cb288c29b7138b290aa354863293e
Yann Santana <testeyann@hotmail.com>
2022-10-13 23:30:35 -0300

    gerador

--- sprint4.java
+++ sprint4.java
@@ -4,26 +4,35 @@ import java.util.Scanner;
 public class sprint4 {
     public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
-        int qtdEqp, auxMa, auxMe;
+        int qtdEqp;
         Random gera = new Random();
+        Random gera2 = new Random();
 
-        System.out.println("Informe a quantidade de equipes para o combate TODOS X TODOS");
-        qtdEqp=key.nextInt();
+        System.out.println("Informe a quantidade de equipes para o combate TODOS X TODOS (a caso houver empate a nota de desing do robo será usada como critério de desempate)");
+        qtdEqp = key.nextInt();
 
-        int[]numEqp=new int[qtdEqp];
-        int[]qtdP=new int[qtdEqp];
+        int[] qtdP = new int[qtdEqp];
+        int[] pDesing = new int[qtdEqp];
+
         for (int i = 0; i < qtdEqp; i++) {
-            numEqp[i]=(i+1);
-            qtdP[i]=gera.nextInt(1,10);
-            System.out.println("equipe "+(numEqp[i])+" obteve "+ qtdP[i]+" pontos no primeiro combate");
+            for (int j = i + 1; j < qtdEqp; j++) {
+                qtdP[i] = gera.nextInt(1, 10);
+                qtdP[j] = gera.nextInt(1, 10);
+                System.out.println(
+                        " equipe " + (i + 1) + " fez " + qtdP[i] + " Pontos x equipe " + (j + 1) + " fez "+qtdP[j]+" Pontos");
+                System.out.println("");
+                if (qtdP[i] == qtdP[j]) {
+                    for(int k=0; k<2;k++){
+                    pDesing[i]=gera.nextInt(1,10);
+                    if (pDesing[i]>pDesing[i+1]) {
+                        qtdP[i]+=1;
+                    }else{
+                        qtdP[j]+=1;
+                    }
+                }
+                }
+            }
         }
-       for (int i = 0; i < qtdP[i]; i++) {
-        if (qtdP[i]>qtdP[i+1]||qtdP[i+1]>qtdP[i]) {
-            System.out.println("Em "+(i+1)+"° Lugar equipe "+(numEqp[i])+" com "+qtdP[i]+" Pontos");
-        }
-       }
-
-
 
     }
 }
