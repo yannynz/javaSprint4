cb56b7b17a59c8b7c7652ea79c4c05213bece884
Yann Santana <testeyann@hotmail.com>
2022-10-14 21:42:14 -0300

    Jesus prfv

--- sprint4.java
+++ sprint4.java
@@ -4,49 +4,61 @@ import java.util.Scanner;
 public class sprint4 {
     public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
-        int qtdEqp, aux, aux1, aux2;
+        int qtdEqp, resultCombat;
         Random gera = new Random();
-        Random gera2 = new Random();
-
         System.out.println(
                 "Informe a quantidade de equipes para o combate TODOS X TODOS (a caso houver empate a nota de desing do robo será usada como critério de desempate)");
         qtdEqp = key.nextInt();
 
         int[] qtdP = new int[qtdEqp];
+        int[] qtdP1 = new int[qtdEqp];
+        int[] winEqp = new int[qtdEqp];
         int[] pDesing = new int[qtdEqp];
         int[] numEquipe = new int[qtdEqp];
         for (int i = 0; i < qtdEqp; i++) {
             for (int j = i + 1; j < qtdEqp; j++) {
                 qtdP[i] = gera.nextInt(1, 10);
                 qtdP[j] = gera.nextInt(1, 10);
+                pDesing[i] = gera.nextInt(1, 10);
+                pDesing[j] = gera.nextInt(1, 10);
                 System.out.println(
                         " equipe " + (numEquipe[i] = i + 1) + " fez " + qtdP[i] + " Pontos x equipe "
                                 + (numEquipe[i] = j + 1) + " fez " + qtdP[i + 1]
                                 + " Pontos");
-                System.out.println("");
-                pDesing[i] = gera.nextInt(1, 10);
-                if (pDesing[i] > pDesing[i + 1]) {
-                    qtdP[i] += 1;
-                } else {
-                    qtdP[j] += 1;
+                if (qtdP[i] == qtdP[i + 1]) {
+                    if (pDesing[i] > pDesing[j]) {
+                        qtdP[i] = qtdP[i]++;
+                    } else {
+                        qtdP[j] = qtdP[j]++;
+                    }
                 }
+                if (qtdP[i] > qtdP[i + 1]) {
+                    System.out.println("Equipe vencedora: " + (numEquipe[i] = (i + 1)));
+                    winEqp[i] = numEquipe[i] = (i + 1);
+                } else if (qtdP[i] < qtdP[i + 1]) {
+                    System.out.println("Equipe vencedora: " + (numEquipe[i] = (j + 1)));
+                    winEqp[i] = numEquipe[i] = (j + 1);
+                }
+                System.out.println("");
             }
         }
-        for (int i = 0; i < qtdP.length; i++) {
-            aux = qtdP[i];
-            qtdP[i] = qtdP[i + 1];
-            qtdP[i + 1] = aux;
+        System.out.println("Proximo combate será entre os melhores da rodada anterior: ");
+        for (int i = 0; i < winEqp.length; i++) {
+            for (int j = i+1; j <= winEqp.length; j++) {
+                qtdP1[j] = gera.nextInt(1, 2);
+                if (qtdP1[j]==1) {
+                    if (pDesing[i] > pDesing[j]||pDesing[i] < pDesing[j]) {
+                        qtdP1[i] = qtdP1[i]+1;
+                    } else {
+                        qtdP1[i+1] = qtdP1[i]+1;
+                    }
+                }
+                System.out.println(
+                        " equipe " + (winEqp[i] = i + 1) + " fez " + qtdP1[i] + " Pontos x equipe "
+                                + (winEqp[i] = j + 1) + " fez " + qtdP1[i + 1]
+                                + " Pontos");
+            }
 
-            aux2 = numEquipe[i];
-            numEquipe[i] = numEquipe[i + 1];
-            numEquipe[i + 1] = aux2;
-        }
-
-        System.out.println("\n Ranking: (Em caso de empate, a nota de design do robô será quesito de desempate) ");
-        System.out.println();
-        for (int i = 0; i < qtdP.length; i++) {
-            System.out.println("#" + (i + 1) + " equipe: " + numEquipe[i] + " | Pontos de Combate: " + qtdP[i]
-                    + " | Nota de design: " + pDesing[i]);
         }
 
     }
