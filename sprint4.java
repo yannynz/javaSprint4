0c3fd1c7cfaf6c9e891fed325ebfd93fbc5bb0d1
Yann Santana <testeyann@hotmail.com>
2022-10-14 23:47:43 -0300

    final

--- sprint4.java
+++ sprint4.java
@@ -4,62 +4,58 @@ import java.util.Scanner;
 public class sprint4 {
     public static void main(String[] args) {
         Scanner key = new Scanner(System.in);
-        int qtdEqp, resultCombat;
+        int qtdEqp;
         Random gera = new Random();
         System.out.println(
                 "Informe a quantidade de equipes para o combate TODOS X TODOS (a caso houver empate a nota de desing do robo será usada como critério de desempate)");
         qtdEqp = key.nextInt();
 
         int[] qtdP = new int[qtdEqp];
-        int[] qtdP1 = new int[qtdEqp];
         int[] winEqp = new int[qtdEqp];
         int[] pDesing = new int[qtdEqp];
         int[] numEquipe = new int[qtdEqp];
+        int[] cont = new int[qtdEqp];
+
         for (int i = 0; i < qtdEqp; i++) {
             for (int j = i + 1; j < qtdEqp; j++) {
                 qtdP[i] = gera.nextInt(1, 10);
                 qtdP[j] = gera.nextInt(1, 10);
-                pDesing[i] = gera.nextInt(1, 10);
-                pDesing[j] = gera.nextInt(1, 10);
-                System.out.println(
-                        " equipe " + (numEquipe[i] = i + 1) + " fez " + qtdP[i] + " Pontos x equipe "
-                                + (numEquipe[i] = j + 1) + " fez " + qtdP[i + 1]
+                pDesing[i] = gera.nextInt(1, 1000);
+                pDesing[j] = gera.nextInt(1, 1000);
+                System.out.println(" equipe " + (numEquipe[i] = i + 1) + " fez " + qtdP[i] + " Pontos x equipe "
+                                + (numEquipe[i] = j + 1) + " fez " + qtdP[j]
                                 + " Pontos");
-                if (qtdP[i] == qtdP[i + 1]) {
-                    if (pDesing[i] > pDesing[j]) {
-                        qtdP[i] = qtdP[i]++;
+                if (qtdP[i] == qtdP[j]) {
+                    if (pDesing[i] > pDesing[i+1]) {
+                        qtdP[i] = qtdP[i]+1;
                     } else {
-                        qtdP[j] = qtdP[j]++;
+                        qtdP[j] = qtdP[j]+1;
                     }
                 }
-                if (qtdP[i] > qtdP[i + 1]) {
+                if (qtdP[i] > qtdP[j]) {
                     System.out.println("Equipe vencedora: " + (numEquipe[i] = (i + 1)));
-                    winEqp[i] = numEquipe[i] = (i + 1);
-                } else if (qtdP[i] < qtdP[i + 1]) {
+                   
+                        winEqp[i] = numEquipe[i] = (i + 1);
+                        cont[i]=(i+1);
+                    
+                } else if (qtdP[i] < qtdP[j]) {
                     System.out.println("Equipe vencedora: " + (numEquipe[i] = (j + 1)));
-                    winEqp[i] = numEquipe[i] = (j + 1);
+                        winEqp[i] = numEquipe[i] = (j + 1);
+                        cont[i]=(i+1);
+                    
                 }
                 System.out.println("");
             }
         }
-        System.out.println("Proximo combate será entre os melhores da rodada anterior: ");
-        for (int i = 0; i < winEqp.length; i++) {
-            for (int j = i+1; j <= winEqp.length; j++) {
-                qtdP1[j] = gera.nextInt(1, 2);
-                if (qtdP1[j]==1) {
-                    if (pDesing[i] > pDesing[j]||pDesing[i] < pDesing[j]) {
-                        qtdP1[i] = qtdP1[i]+1;
-                    } else {
-                        qtdP1[i+1] = qtdP1[i]+1;
-                    }
-                }
-                System.out.println(
-                        " equipe " + (winEqp[i] = i + 1) + " fez " + qtdP1[i] + " Pontos x equipe "
-                                + (winEqp[i] = j + 1) + " fez " + qtdP1[i + 1]
-                                + " Pontos");
-            }
-
+        System.out.println("Proximo combate será entre: ");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(winEqp[i]);
         }
+        
+        
+// ta incompleto professor, desculpa, tenha compaixão por favor. 
+        
+                
 
     }
 }
